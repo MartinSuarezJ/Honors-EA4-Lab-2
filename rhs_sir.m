f8e3c750b1348f175a9a79f2682d564206f7953d
Martin Suarez <martinsuarezjaramillo@gmail.com>
2026-04-26 14:11:45 -0500

    removed unwanted files

--- rhs_sir.m
+++
@@ -1,8 +0,0 @@
-function yp = rhs_sir(t, y)
-    % Function yp solves the right hand side of equation 6.
-    alpha = 1;
-    beta = 2;
-    s0 = 0.99;
-
-    yp = alpha * (1 - y - s0*exp((beta/alpha)*y));
-end
--- rhs_sis.m
+++
@@ -1,7 +0,0 @@
-function yp = rhs_sis(t, y)
-    % Function yp solves the right hand side of equation 5.
-    alpha = 1;
-    beta = 2;
-
-    yp = beta * y * ((1-alpha/beta-y));
-end
